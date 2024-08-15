245b734a9c93083feec574e7c586abf622872962
Brandon Sicay <bsicay@pacifiko.com>
2024-08-15 13:28:36 -0600

    sockets for floding

--- Flooding.py
+++ Flooding.py
@@ -1,14 +1,13 @@
-"""
-Universidad del Valle de Guatemala
-Algoritmos de Enrutamiento
-"""
-
 import json
+import socket
+import threading
 from node import Node
 
 class Flooding():
     def __init__(self):
         self.nodes = []
+        self.node_ports = {}
+        self.actual_node = None
 
     def add_node(self, node):
         self.nodes.append(node)
@@ -34,131 +33,144 @@ class Flooding():
 
             # Guardar todos los nodos creados
             self.nodes = list(node_dict.values())
-        
+
+    def load_ports(self, filename):
+        with open(filename, 'r') as file:
+            self.node_ports = json.load(file)
+
     def flood(self, source_node, message):
         message_data = json.loads(message)
         message_type = message_data["type"]
         headers = message_data["headers"]
-        
-        if(source_node.name not in headers["receivers"]):
+
+        if source_node.name not in headers["receivers"]:
             headers["receivers"].append(source_node.name)
-        
-            for neighbor in source_node.get_neighbors():   
-                if(neighbor.name not in headers["receivers"]):
-                    if(neighbor.name != headers["to"]):
+
+            for neighbor in source_node.get_neighbors():
+                if neighbor.name not in headers["receivers"]:
+                    if neighbor.name != message_data["to"]:
                         headers["receivers"].append(neighbor.name)
-                    if headers['hop_count'] > 0:
+                    if message_data['hop_count'] > 0:
                         print(f"Reenvía este paquete a: {neighbor.name}")
-                        headers['hop_count'] -= 1
+                        message_data['hop_count'] -= 1
                         message = json.dumps(message_data, indent=4)
                         print(message)
-                        print()
-                        self.flood(neighbor, message)
+                        self.send_message(neighbor.name, message)
                     else:
-                        print(f"El hop count ha expirado para el mensaje a {headers['to']} en {neighbor.name}")
-
+                        print(f"El hop count ha expirado para el mensaje a {message_data['to']} en {neighbor.name}")
 
     def initiate_flood(self, source_node, message_data, destiny, hc):
         message = self.create_message(source_node, message_data, destiny, hc)
         self.flood(source_node, message)
-            
+
     def create_message(self, source_node, message_data, destiny, hc):
         headers = {
-            "from": source_node.name,
-            "to": destiny.name,
-            "hop_count": hc,
             "receivers": []
         }
-        
+
         payload = message_data
 
         message = {
             "type": "message",
+            "from": source_node.name,
+            "to": destiny.name,
+            "hop_count": hc,
             "headers": headers,
             "payload": payload
         }
-        
+
         return json.dumps(message, indent=4)
-    
+
     def process_message(self, message, receiving_node):
         message_data = json.loads(message)
         message_type = message_data["type"]
-        headers = message_data["headers"]
 
-        if(receiving_node.name == headers['to']):
-            if(message_type == "info"):
-                print("Mensaje de información recibida:", headers)
-                print()
-            elif(message_type == "message"):
-                print(f"({receiving_node.name}) Mensaje entrante de: {headers['from']}")
+        if receiving_node.name == message_data['to']:
+            if message_type == "info":
+                print("Mensaje de información recibida:", message_data)
+            elif message_type == "message":
+                print(f"({receiving_node.name}) Mensaje entrante de: {message_data['from']}")
                 print(message_data["payload"], "\n")
 
-        for neighbor in receiving_node.get_neighbors():  
-            if(neighbor.name not in headers["receivers"]):
-                if(neighbor.name != headers["to"]):
-                    headers["receivers"].append(neighbor.name)
-                if headers['hop_count'] > 0:
+        for neighbor in receiving_node.get_neighbors():
+            if neighbor.name not in message_data["headers"]["receivers"]:
+                if neighbor.name != message_data["to"]:
+                    message_data["headers"]["receivers"].append(neighbor.name)
+                if message_data['hop_count'] > 0:
                     print(f"Reenvía este paquete a: {neighbor.name}")
-                    headers['hop_count'] -= 1
+                    message_data['hop_count'] -= 1
                     message = json.dumps(message_data, indent=4)
-                    print(message)
-                    print()
-                    self.flood(neighbor, json.dumps(message_data, indent=4))
+                    self.send_message(neighbor.name, message)
                 else:
-                    print(f"El hop count ha expirado para el mensaje a {headers['to']} en {neighbor.name}")
-            
+                    print(f"El hop count ha expirado para el mensaje a {message_data['to']} en {neighbor.name}")
+
+    def send_message(self, neighbor_name, message):
+        port = self.node_ports.get(neighbor_name)
+        if port:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect(('localhost', port))
+                s.sendall(message.encode('utf-8'))
+
+    def handle_connection(self, conn, addr):
+        with conn:
+            message = conn.recv(1024).decode('utf-8')
+            print(f"Mensaje recibido de {addr}: {message}")
+            self.process_message(message, self.actual_node)
+
+    def start_server(self):
+        port = self.node_ports[self.actual_node.name]
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind(('localhost', port))
+            s.listen()
+            print(f"Nodo {self.actual_node.name} escuchando en el puerto {port}...")
+            while True:
+                conn, addr = s.accept()
+                threading.Thread(target=self.handle_connection, args=(conn, addr)).start()
+
     def start(self):
-        print()
-        topology_file = input("Ingrese el nombre del archivo de topología (e.g., topo-1.txt): ")
+        # topology_file = input("Ingrese el nombre del archivo de topología (e.g., topo-1.txt): ")
+        # ports_file = input("Ingrese el nombre del archivo de puertos (e.g., ports.json): ")
+        topology_file = "topologia.txt"
+        ports_file =  "ports.json"
         self.load_topology(topology_file)
+        self.load_ports(ports_file)
 
         name = input("Nombre del nodo actual: ")
-        actual_node = None
+        self.actual_node = None
 
         # Encontrar el nodo correspondiente al nombre dado
         for node in self.nodes:
             if node.name == name.upper():
-                actual_node = node
+                self.actual_node = node
                 break
 
-        if actual_node is None:
+        if self.actual_node is None:
             print(f"Error: Nodo {name} no encontrado en la topología.")
             return
 
+        # Iniciar el servidor para este nodo en un hilo separado
+        threading.Thread(target=self.start_server, daemon=True).start()
+
         print("\nDatos cargados de la topología\n")
-        for el in self.nodes:
-            print(el)
+        # for el in self.nodes:
+        #     print(el)
 
         ver2 = False
-        while(not ver2):
+        while not ver2:
             print("\n1) Enviar mensaje")
-            print("2) Recibir mensaje")
-            print("3) Salir")
+            print("2) Salir")
             op = int(input("Ingresa opción: "))
-            
+
             match op:
                 case 1:
-                    sour = actual_node
+                    sour = self.actual_node
                     dest = input("Nombre de nodo destino: ")
                     msg = input("Ingresa el mensaje que deseas enviar: ")
                     hc = int(input("Ingresa el hop_count: "))
-                    
-                    print()
-                    
+
                     node_dest = Node(dest.upper())
                     self.initiate_flood(sour, msg, node_dest, hc)
-                    
+
                 case 2:
-                    print("Ingresa el mensaje recibido (Doble Enter para confirmar)")
-                    lines = []
-                    while True:
-                        line = input()
-                        if line == "":
-                            break
-                        lines.append(line)
-                    message_json = "\n".join(lines)   
-                    self.process_message(message_json, actual_node)         
-                    
-                case 3:
                     print("Entendido, saliendo...")
                     ver2 = True
